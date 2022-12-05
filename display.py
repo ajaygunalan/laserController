95315de28012dbaabbb9fc56f2f84871b1770ae2
Ajay Gunalan <ajaygunalan1995@gmail.com>
2022-12-05 13:58:46 +0100

    basic refactoring works fine

--- display.py
+++ display.py
@@ -1,5 +1,5 @@
 import cv2
-input_video_path = "data/basicShadow.mp4"
+input_video_path = "./data/basicShadow.mp4"
 vid = cv2.VideoCapture(input_video_path)
 while(True):
     ret, frame = vid.read()
--- laserTracker.py
+++ laserTracker.py
@@ -6,48 +6,22 @@ import numpy as np
 from scipy import ndimage
 from collections import deque
 
-def getCentre():
-    _, frame = cap.read()
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+#Gloal Variable
+queueSize = 10
+qX = deque(maxlen=queueSize)
+qY = deque(maxlen=queueSize)
 
-    # Checks if array elements lie between lower & upper red.
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-
-    # Find all the contours in the binary mask
-    contours,hierarchy = cv2.findContours(mask,2,1)
-
-    big_contour = []
-    max = 0
-    cX = 0
-    cY = 0
-
-    for i in contours:
-        area = cv2.contourArea(i) #--- find the contour having biggest area ---
-        if(area > max):
-            max = area
-            big_contour = i
-            # compute the center of the contour
-            M = cv2.moments(big_contour)
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
-
-    return [cX, cY], frame, big_contour, mask
-
-
-def initFilter(N):
-    for i in range(0, N):
-        centre, _, _, _ = getCentre()
-        qX.append(centre[0])
-        qY.append(centre[1])
-
+def init():
+    for i in (0, queueSize):
+        qX.append(i)
+        qY.append(i)
 
 def lowPassFilter(center):
-
     qX.append(center[0])
     qY.append(center[1])
 
-    FilterValX = ndimage.median_filter(qX, size=FilterSize)
-    FilterValY = ndimage.median_filter(qY, size=FilterSize)
+    FilterValX = ndimage.median_filter(qX, size=5)
+    FilterValY = ndimage.median_filter(qY, size=5)
 
     center[0] = sum(FilterValX)/len(FilterValX)
     center[1] = sum(FilterValY)/len(FilterValY)
@@ -58,39 +32,55 @@ def lowPassFilter(center):
     return tuple(center)
 
 
-# Parametrs
-queueSize = 60
-FilterSize = 5
-qX = deque(maxlen=queueSize)
-qY = deque(maxlen=queueSize)
+def main():
+    lower_red = np.array([0, 0, 255])
+    upper_red = np.array([255, 255, 255])
 
-# Drift offset need to be hand tuned by us.
-offset1 = 10
-offset2 = 20
+    input_video_path = "./data/basicShadow.mp4"
+    cap = cv2.VideoCapture(input_video_path)
 
-lower_red = np.array([0, 0, 255])
-upper_red = np.array([255, 255, 255])
+    init()
 
-input_video_path = "data/basicShadow.mp4"
-# input_video_path = 4
-cap = cv2.VideoCapture(input_video_path)
+    while (1):
+        _, frame = cap.read()
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-initFilter(queueSize) # 1 SOR 2SECONDS
-while (1):
-    [x, y], frame, contour, mask = getCentre()
-    (xFilt, yFilt) = lowPassFilter([x,y])
+        # Checks if array elements lie between lower & upper red.
+        mask = cv2.inRange(hsv, lower_red, upper_red)
 
-    frameFilter = frame
-    cv2.circle(mask, tuple([x,y]), 20, (0, 0, 255), 2, cv2.LINE_AA)
-    cv2.drawContours(mask, contour, -1, (0,255,0), 3)
+        # Find all the contours in the binary mask
+        contours,hierarchy = cv2.findContours(mask,2,1)
 
-    cv2.circle(frameFilter, (xFilt, yFilt) , 20, (0, 0, 255), 2, cv2.LINE_AA)
-    cv2.drawContours(frameFilter, contour, -1, (0,255,0), 3)
+        big_contour = []
+        max = 0
+        cX = 0
+        cY = 0
 
-    cv2.imshow('mask', mask)
-    cv2.imshow('Filterd', frameFilter)
+        for i in contours:
+            area = cv2.contourArea(i) #--- find the contour having biggest area ---
+            if(area > max):
+                max = area
+                big_contour = i
+                # compute the center of the contour
+                M = cv2.moments(big_contour)
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        (xFilt, yFilt) = lowPassFilter([cX,cY])
 
-cap.release()
+        frameFilter = frame
+        cv2.circle(mask, tuple([cX,cY]), 20, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.drawContours(mask, big_contour, -1, (0,255,0), 3)
+
+        cv2.circle(frameFilter, (xFilt, yFilt) , 20, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.drawContours(frameFilter, big_contour, -1, (0,255,0), 3)
+
+        cv2.imshow('mask', mask)
+        cv2.imshow('Filterd', frameFilter)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+
+if __name__=="__main__":
+    main()
